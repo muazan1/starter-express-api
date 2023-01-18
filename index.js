025634f6161f05c20c44cecf051e1eab21c36b03
Muazan <muazan.stallyons@gmail.com>
2023-01-18 17:41:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const express = require('express')
 const app = express()
 const port = process.env.APP_LISTEN_PORT || 8080
 const bodyParser = require('body-parser')
-const Routes = require('./routes/index')
-const ReportRoutes = require('./routes/reports/index')
-const TokenRoutes = require('./routes/crypto/index')
 
-
-const NewsLetterRoutes = require('./routes/newsletter/index')
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-app.use(bodyParser.json());
+var Routes = require('./routes/index')
+var ReportRoutes = require('./routes/reports/index')
+var TokenRoutes = require('./routes/crypto/index')
+var NewsLetterRoutes = require('./routes/newsletter/index')
 
 console.log('Routes')
 app.use('/', Routes)
@@ -26,6 +23,8 @@ app.use(`${process.env.API_URL}/news-letters`, NewsLetterRoutes)
 
 const cors = require('cors');
 
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(bodyParser.json());
 app.use(cors({
     origin: 'http://127.0.0.1:3000',
     methods: ["GET", 'POST']
