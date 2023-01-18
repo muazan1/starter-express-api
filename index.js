1cc3a9bac279ff81bbb80b79833f8ff67e007ef3
Muazan <muazan.stallyons@gmail.com>
2023-01-18 18:02:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const port = process.env.APP_LISTEN_PORT || 8080
 const bodyParser = require('body-parser')
 
 var Routes = require('./routes/index')
-var ReportRoutes = require('./routes/reports/index')
-var TokenRoutes = require('./routes/crypto/index')
-var NewsLetterRoutes = require('./routes/newsletter/index')
+// var ReportRoutes = require('./routes/reports/index')
+// var TokenRoutes = require('./routes/crypto/index')
+// var NewsLetterRoutes = require('./routes/newsletter/index')
 
 console.log('Routes')
 app.use('/', Routes)
-console.log('ReportRoutes')
-app.use(`${process.env.API_URL}/reports`, ReportRoutes)
-console.log('TokenRoutes')
-app.use(`${process.env.API_URL}/crypto_tokens`, TokenRoutes)
-console.log('NewsLetterRoutes')
-app.use(`${process.env.API_URL}/news-letters`, NewsLetterRoutes)
+// console.log('ReportRoutes')
+// app.use(`${process.env.API_URL}/reports`, ReportRoutes)
+// console.log('TokenRoutes')
+// app.use(`${process.env.API_URL}/crypto_tokens`, TokenRoutes)
+// console.log('NewsLetterRoutes')
+// app.use(`${process.env.API_URL}/news-letters`, NewsLetterRoutes)
 
 const cors = require('cors');
 
