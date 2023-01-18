602b8ea480ff38e99cc155aa069c85aa03d1cbbc
Muazan <muazan.stallyons@gmail.com>
2023-01-18 16:24:20 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const Router = express.Router()
 
+
+Router.get('/', (req, res) => {
+    res.send('App is Wokring')
+});
 // checking auth
 Router.get(`${process.env.API_URL}/check`, async (req, res) => {
     res.send('Working')
