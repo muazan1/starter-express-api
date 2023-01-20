9f220b94ad7dda67b002882868845dba9f1f7f57
Muazan <muazan.stallyons@gmail.com>
2023-01-20 16:29:07 +0500

    updated cors

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({ origin: 'https://wisesama.netlify.app' }))
+app.use(cors({ origin: '*' }))
+app.options('*', cors());
 // app.use(cors({
 //     origin: '*',
 //     methods: ["GET", 'POST', 'PUT', 'DELETE']
