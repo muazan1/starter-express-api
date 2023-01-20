9a1f66cfa98cd7a57f6cae0688e3865030a81501
Muazan <muazan.stallyons@gmail.com>
2023-01-20 16:17:25 +0500

    updated cors

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-    origin: '*',
-    methods: ["GET", 'POST', 'PUT', 'DELETE']
-}));
+app.use(cors())
+// app.use(cors({
+//     origin: '*',
+//     methods: ["GET", 'POST', 'PUT', 'DELETE']
+// }));
 app.use(helmet())
 
 
